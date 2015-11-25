5c459407853c1dc3234b4167800638530253d689
Haohan Wang <haohanw@andrew.cmu.edu>
2015-11-24 23:50:22 -0500

    1. running example of EM algorithm, 2. fix some format of generating data

--- lmm/LMM.py
+++ lmm/LMM.py
@@ -46,54 +46,91 @@ class LMM:
     def nll_d_sigma(self):
         V = self._v()
         inV = i(V)
-        ym = self.y.T - self.X*self.beta
+        ym = self.y.T - self.X * self.beta
 
         # derivatives
         d_V_D = self.ZZt
         d_b_V = self._nll_b_d_V(inV)
         d_nll_b = -self.X.T
-        d_nll_d = d_nll_b*d_b_V*d_V_D
+        d_nll_d = d_nll_b * d_b_V * d_V_D
 
-        tmp1 = d_nll_d*V*ym + ym.T*d_V_D*ym + ym.T*V*d_nll_d
-        tmp2 = i(V.T)*d_V_D
+        tmp1 = d_nll_d * V * ym + ym.T * d_V_D * ym + ym.T * V * d_nll_d
+        tmp2 = i(V.T) * d_V_D
 
         return tmp1 + tmp2
 
     def _nll_b_d_V(self, inV):
-        xvx = self.X.T*inV*self.X
+        xvx = self.X.T * inV * self.X
         ixvx = i(xvx)
-        xx = self.X.T*self.X
-        tmp1 = np.trace((ixvx*xx*ixvx).T*inV*inV)*self.X.T*inV*self.y
-        tmp2 = ixvx*inV.T*self.X.T*self.y*inV.T
+        tmp1 = ixvx * self.X.T * inV * inV * self.X * ixvx * self.X.T * inV * self.y.T
+        tmp2 = ixvx * self.X.T * inV.T * inV.T * self.y.T
         return tmp1 - tmp2
 
     def sgd(self):
         D = self._d()
         for epoch in range(self.epochs):
             grad = self.nll_d_sigma()
-            D = D - self.step_size*grad
+            D = D - self.step_size * grad
+
+    # def em(self):
+    #     V = self._v()
+    #     prev_ll = self.log_likelihood()
+    #     print prev_ll
+    #     for epoch in range(self.epochs):
+    #         # ym = self.y.T - self.X*self.beta
+    #         #grad = self.Z.T*ym*ym.T*self.Z + self.Z.T*(i(V.T))*self.Z
+    #         self.D = self._d()
+    #         V = self._v()
+    #         inV = i(V)
+    #         self.beta = i(self.X.T*inV*self.X)*self.X.T*inV*self.y.T
+    #         self.mu = self.D*self.Z.T*inV*(self.y.T - self.X*self.beta)
+    #         # print self.beta.mean()
+    #         ll = self.log_likelihood()[0,0]
+    #         print ll
 
     def em(self):
-        V = self._v()
         prev_ll = self.log_likelihood()
         print prev_ll
+        sigma = np.square(self.mu)
+        sig = sigma.T.tolist()[0]
+        # print sig
+
+        zshp = self.Z.shape
+
+        V = self._v()
+        inV = i(V)
+        Py = None
         for epoch in range(self.epochs):
-            ym = self.y.T - self.X*self.beta
-            grad = self.Z.T*ym*ym.T*self.Z + self.Z.T*(i(V.T)).T*self.Z
-            self.D += self.step_size*grad
+            P = inV - inV * self.X * i(self.X.T * inV * self.X) * self.X.T * inV
+            yP = self.y * P
+            Py = P * self.y.T
+
+            for j in range(zshp[1]):
+                sig[j] += (sig[j] ** 2) * \
+                          (yP * self.Z[:, j] * self.Z[:, j].T * Py - np.trace(self.Z[:, j].T * inV * self.Z[:, j]))[
+                              0, 0]
+            self.D = np.diag(sig)
             V = self._v()
             inV = i(V)
-            self.beta = i(self.X.T*inV*self.X)*self.X.T*inV*self.y.T
-            self.mu = self.D*self.Z.T*inV*(self.y.T - self.X*self.beta)
-            # print self.beta.mean()
-            ll = self.log_likelihood()[0,0]
+            ll = self.log_likelihood()
             print ll
+            # print sig
+        self.beta = i(self.X.T * inV * self.X) * self.X.T * inV * self.y.T
+        self.mu = self.D * self.Z.T * Py
 
+    def clasify(self, X, Z):
+        y_pred = expit(X * self.beta)
+        m = np.mean(y_pred)
+        ind = np.where(y_pred <= m)
+        y_pred[ind] = 0
+        ind = np.where(y_pred > m)
+        y_pred[ind] = 1
+        return y_pred
 
-    def predict(self, X, Z):
-        return expit(X * self.beta + Z * self.mu)
+    def regress(self, X, Z):
+        return X*self.beta
 
-    def train(self, X, Z, y, method='EM', cost='ML', epochs=0, step_size=1e-10):
+    def train(self, X, Z, y, method='EM', cost='ML', epochs=5, step_size=1):
         '''
         :param X: fixed effect input
         :param Z: random effect input
@@ -135,17 +172,14 @@ class LMM:
 if __name__ == '__main__':
     import pickle
 
-    data = pickle.load(open('../data/synthetic.pkl'))
+    data = pickle.load(open('../data/pig.pkl'))
     X = data['X']
     Z = data['Z']
     y = data['y']
     lmm = LMM()
     lmm.train(X, Z, y, method='EM')
-    y_pred = lmm.predict(X, Z)
-    ind = np.where(y_pred<=0.5)
-    y_pred[ind] = 0
-    ind = np.where(y_pred>0.5)
-    y_pred[ind] = 1
-    print y_pred
-    shp = y.shape
-    print len(np.where(y_pred.reshape(shp) == y)[0])/float(shp[0])
+    y_pred = lmm.regress(X, Z)
+    # print y_pred
+    # shp = y.shape
+    # print len(np.where(y_pred.reshape(shp) == y)[0]) / float(shp[0])
+    print np.square(y-y_pred).mean()
--- utility/syntheticData.py
+++ utility/syntheticData.py
@@ -2,6 +2,14 @@ __author__ = 'haohanwang'
 
 import numpy as np
 import pickle
+from matplotlib import pyplot as plt
+
+from scipy.special import expit
+from scipy.stats import logistic
+
+def sigmoid(z):
+    s = 1.0 / (1.0 + np.exp(-1.0 * z))
+    return np.array(s)
 
 
 def save_object(obj, filename):
@@ -9,29 +17,96 @@ def save_object(obj, filename):
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
 
-n = 1000
-d = 100
-e = 50
+def original():
+    n = 1000
+    d = 100
+    e = 50
 
-beta = np.random.random(d)
-beta[np.where(beta<=0.5)] = 0
-beta = np.matrix(beta)
-mu = np.matrix(np.random.random(50))
-D = np.std(mu)
+    beta = np.random.random(d)
+    beta[np.where(beta<=0.5)] = 0
+    beta = np.matrix(beta)
+    mu = np.matrix(np.random.random(e))
+    D = np.std(mu)
 
-X = np.matrix(np.random.random((n, d)))
-Z = np.matrix(np.random.random((n, e)))
-y = np.zeros(n)
-for i in range(n):
-    y[i] = np.random.normal(X[i,:]*beta.T, Z[i,:]*D*Z[i,:].T)
+    X = np.matrix(np.random.random((n, d)))
+    Z = np.matrix(np.random.random((n, e)))
+    y = np.zeros(n)
+    for i in range(n):
+        y[i] = np.random.normal(X[i,:]*beta.T, Z[i,:]*D*Z[i,:].T)
 
-m = np.mean(y)
-y[np.where(y<=m)] = 0
-y[np.where(y>m)] = 1
+    m = np.mean(y)
+    y[np.where(y<=m)] = 0
+    y[np.where(y>m)] = 1
 
-data = {}
-data['X'] = X
-data['Z'] = Z
-data['y'] = y
+    data = {}
+    data['X'] = X
+    data['Z'] = Z
+    data['y'] = y
 
-save_object(data, '../data/synthetic.pkl', )
+    y_pred = sigmoid(X*beta.T)
+    ind = np.where(y_pred<=0.5)
+    y_pred[ind] = 0
+    ind = np.where(y_pred>0.5)
+    y_pred[ind] = 1
+    shp = y.shape
+    print len(np.where(y_pred.reshape(shp) == y)[0])/float(shp[0])
+
+    # save_object(data, '../data/synthetic.pkl', )
+
+def new_smaller():
+    n = 100
+    d = 10
+    e = 2
+
+    beta = np.random.random(d)
+    beta[np.where(beta<=0.5)] = 0
+    beta = np.matrix(beta)
+    mu = np.matrix(np.random.random(e))
+    D = np.std(mu)
+
+    X = np.matrix(np.random.random((n, d)))
+    Z = np.matrix(np.random.random((n, e)))
+
+    y = X*beta.T
+
+    m = np.mean(y)
+    y[np.where(y<=m)] = 0
+    y[np.where(y>m)] = 1
+
+
+    y_pred = sigmoid(X*beta.T)
+    # print y_pred
+    m = np.mean(y_pred)
+    ind = np.where(y_pred<=m)
+    y_pred[ind] = 0
+    ind = np.where(y_pred>m)
+    y_pred[ind] = 1
+    shp = y.shape
+    print len(np.where(y_pred.reshape(shp) == y)[0])/float(shp[0])
+
+def existing():
+    import statsmodels.api as sm
+    data = sm.datasets.get_rdataset('dietox', 'geepack').data
+    weights = data['Weight']
+    w = []
+    t = []
+    p = []
+    for i in range(len(weights)):
+        w.append(weights[i])
+        t.append(data['Time'][i])
+        p.append(data['Pig'][i])
+
+    # y = np.array(w)
+    # X = np.matrix(np.array(t))
+    # Z = np.matrix(np.array(p))
+    # data = {}
+    # data['X'] = X.T
+    # data['Z'] = Z.T
+    # data['y'] = y.T
+    # save_object(data, '../data/pig.pkl', )
+    plt.scatter(t, w)
+    plt.show()
+
+
+if __name__ == '__main__':
+    existing()
